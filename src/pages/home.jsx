5b510a8b702b336b82eb14ebe049da9e90bb70b8
SadraDinAziziMuhammad <sadraazizi1305@gmail.com>
2024-09-03 21:53:34 +0700

    Hero Section

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,19 +1,30 @@
 import React from "react";
+import heroImage1 from "/public/img/Dummy/Dummy1.jpeg";
+import { Carousel } from "react-bootstrap";
+import "/src/App.css"
 
 export const Home = () => {
   return (
-    <div>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora nesciunt
-      labore sapiente esse culpa itaque corporis. Rem ratione veritatis porro
-      nulla ab nihil adipisci dignissimos non repellendus officiis laboriosam,
-      maiores possimus similique. Beatae, est? Quasi nisi commodi laborum saepe,
-      facilis laudantium unde eum quos nemo accusantium repellat aut facere
-      repellendus consequatur incidunt harum corrupti exercitationem sequi
-      voluptatem, inventore quia recusandae. Perspiciatis iure voluptatibus
-      sequi! Sunt quasi temporibus natus ab dicta, suscipit enim quidem nobis
-      veritatis in atque quaerat fugiat ipsum nostrum dolore iste. Pariatur quo,
-      perspiciatis aperiam sint veniam ipsa, minima quas facere cum dicta labore
-      sit? Similique, laboriosam nostrum.
+    <div className="homepage">
+      <header className="header-box">
+        <Carousel>
+          <Carousel.Item>
+            <img className="d-block 2-100" 
+            src={heroImage1} 
+            alt="Image-Header" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block 2-100" 
+            src={heroImage1} 
+            alt="Image-Header" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block 2-100" 
+            src={heroImage1} 
+            alt="Image-Header" />
+          </Carousel.Item>
+        </Carousel>
+      </header>
     </div>
   );
 };
